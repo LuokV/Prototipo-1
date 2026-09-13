8b6eed9c4b4f671b2754d1a4890f3caf66d262a1
LuokV <lvnoka@live.com>
2026-09-13 15:51:31 -0300

    Clase Escenario
    - Se modifico el modulo "escenario" para que se comporte como una clase

--- escenario.lua
+++ escenario.lua
@@ -1,25 +1,20 @@
-Estructuras = {}
-Estructuras.__index = Estructuras
+Estructuras = Class {}
 
 local tag1 = "Pared"
 local tag2 = "Piso"
 local tag3 = "Plataforma"
 
-function Estructuras:Nuevo(x, y, ruta, tag, escalax, escalay, world)
+function Estructuras:init(x, y, ruta, tag, escalax, escalay, world)
 
-    local plataforma = setmetatable({}, Estructuras)
+    self.sprite = love.graphics.newImage(ruta)
+    self.cuerpo = love.physics.newBody(world, x, y)
+    self.escala_x = escalax
+    self.escala_y = escalay
+    self.forma = love.physics.newRectangleShape(self.sprite:getWidth()*self.escala_x, self.sprite:getHeight()*self.escala_y)
+    self.acople = love.physics.newFixture(self.cuerpo, self.forma)
+    self.acople:setUserData(tag)
 
-    plataforma.sprite = love.graphics.newImage(ruta)
-    plataforma.cuerpo = love.physics.newBody(world, x, y)
-    plataforma.escala_x = escalax
-    plataforma.escala_y = escalay
-    plataforma.forma = love.physics.newRectangleShape(plataforma.sprite:getWidth()*plataforma.escala_x, plataforma.sprite:getHeight()*plataforma.escala_y)
-    plataforma.acople = love.physics.newFixture(plataforma.cuerpo, plataforma.forma)
-    plataforma.acople:setUserData(tag)
-
-    plataforma.acople:setFriction (0) -- Evita que el jugador pueda agarrarse a las estructuras
-
-    return plataforma
+    self.acople:setFriction (0) -- Evita que el jugador pueda agarrarse a las estructuras
 
 end
 
@@ -32,18 +27,18 @@ end
 ---- FUNCIONES PARA CARGAR y DIBUJAR EL ESCENARIO RESPECTIVAMENTE
 
 function CrearEscenario(world)
-    columnaizquieda = Estructuras:Nuevo(ventana.ancho-(ventana.ancho-6), ventana.alto/2, "img/Wall.png", tag1, 0.75, 1.38, world)
-    columnaderecha = Estructuras: Nuevo(ventana.ancho-6,ventana.alto/2,"img/Wall.png", tag1, 0.75, 1.38, world)
+    columnaizquieda = Estructuras(ventana.ancho-(ventana.ancho-6), ventana.alto/2, "img/Wall.png", tag1, 0.75, 1.38, world)
+    columnaderecha = Estructuras(ventana.ancho-6,ventana.alto/2,"img/Wall.png", tag1, 0.75, 1.38, world)
 
-    piso = Estructuras:Nuevo(ventana.ancho/2, ventana.alto-5, "img/Floor.png", tag2, 1.5, 1, world)
+    piso = Estructuras(ventana.ancho/2, ventana.alto-5, "img/Floor.png", tag2, 1.5, 1, world)
 
     --plataformas sin sprites
-    centro = Estructuras:Nuevo(ventana.ancho/2, ventana.alto/2 + 5, "img/Floor.png", tag3, 0.25, 0.50, world)
+    centro = Estructuras(ventana.ancho/2, ventana.alto/2 + 5, "img/Floor.png", tag3, 0.25, 0.50, world)
 
-    superior_izq = Estructuras:Nuevo(50, 60, "img/Floor.png", tag3, 0.20, 0.50, world)
-    superior_der = Estructuras:Nuevo(190, 60, "img/Floor.png", tag3, 0.20, 0.50, world)
-    inferior_izq = Estructuras:Nuevo(50, 150, "img/Floor.png", tag3, 0.20, 0.50, world)
-    inferior_der = Estructuras:Nuevo(190, 150, "img/Floor.png", tag3, 0.20, 0.50, world)
+    superior_izq = Estructuras(50, 60, "img/Floor.png", tag3, 0.20, 0.50, world)
+    superior_der = Estructuras(190, 60, "img/Floor.png", tag3, 0.20, 0.50, world)
+    inferior_izq = Estructuras(50, 150, "img/Floor.png", tag3, 0.20, 0.50, world)
+    inferior_der = Estructuras(190, 150, "img/Floor.png", tag3, 0.20, 0.50, world)
 end
 
 --NOTA: El cuerpo FISICO se origina desde el centro, mientras que los SPRITES desde la esquina superior izq
