d6c80626741ca6ecaa97440ec34fae0734d8f581
LuokV <lvnoka@live.com>
2026-09-17 15:39:47 -0300

    Ubicación texto derrota
    - Acomodado los textos luego de la derrota

--- estados_globales/estadoDerrota.lua
+++ estados_globales/estadoDerrota.lua
@@ -8,7 +8,7 @@ function EstadoDerrota:actualizar(dt) end
 function EstadoDerrota:dibujar()
     love.graphics.setFont(fuente)
     love.graphics.setColor(1,0,0)
-    love.graphics.printf('GAME OVER', 0, 64, ventana.ancho * ventana.escala, 'center')
-    love.graphics.printf('Continuar', 0, 104, ventana.ancho * ventana.escala, 'center')
+    love.graphics.printf('GAME OVER', 0, 250, ventana.ancho * ventana.escala, 'center')
+    love.graphics.printf('Continuar, reinicio: ENTER', 0, 350, ventana.ancho * ventana.escala, 'center')
 end
 
